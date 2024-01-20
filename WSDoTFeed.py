3598fc034df4ed7f970b7821b1b600e0515add7c
nechja <kayla.graves@gmail.com>
2024-01-19 23:27:33 -0800

    updates

--- WSDoTFeed.py
+++ WSDoTFeed.py
@@ -1,9 +1,11 @@
 import feedparser
 import json
 import time
+import re
 import requests
 from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
+from datetime import datetime
 
 def save_last_seen_entry(feed_name, last_seen_entry_id):
     with open(f"last_seen_{feed_name}.txt", "w") as file:
@@ -60,13 +62,35 @@ def parse_html(html_string):
     html.date = divs[2].get_text() if len(divs) > 2 else ""
 
     return html;
+
+def parse_html_passes(data_string):
+    name = re.search(r'<strong>(.*?)</strong>', data_string).group(1)
+    datetime_str = re.search(r'<strong>.*?</strong><br />(.*?)<br', data_string).group(1)
+    temperature = re.search(r'<strong>Temperature:</strong>&nbsp;(.*?)&deg;F', data_string).group(1)
+    directions = re.findall(r'<strong>(Eastbound|Westbound|Northbound|Southbound)</strong><br />(.*?)<br', data_string)
+    conditions = re.search(r'<strong>Conditions:</strong><br />(.*?)<br', data_string).group(1)
+    weather = re.search(r'<strong>Weather:</strong><br />(.*?)<br', data_string).group(1)
+
+    # Convert datetime string to datetime object
+    datetime_obj = datetime.strptime(datetime_str, '%m/%d/%Y %I:%M %p')
+
+    # Organizing data into a dictionary
+    extracted_data = {
+        "Name": name,
+        "Datetime": datetime_obj,
+        "Temperature": temperature + "°F",
+        "Directions": directions,
+        "Conditions": conditions,
+        "Weather": weather
+    }
+    return extracted_data
     
-def send_discord_message_road(webhook_url, feed_name, feed_icon, color, tags, image, entry):
+def send_discord_message_road(webhook_url, feed_name, feed_icon, color, tags, entry):
 
 
     html = parse_html(entry.summary)
 
-    if html.impact != "Low":
+    if html.impact == "High Impact":
         data = {
             "username": feed_name,
             "avatar_url": feed_icon,
@@ -83,19 +107,19 @@ def send_discord_message_road(webhook_url, feed_name, feed_icon, color, tags, im
                     }
                 ],
                 "image": {
-                    "url": image
+                    "url": ""
                 }
                 }
 
             ]
         }
     
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
-    if response.status_code == 204:
-        print("Message sent successfully")
-    else:
-        print(f"Failed to send message: {response.status_code}")
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+        if response.status_code == 204:
+            print("Message sent successfully")
+        else:
+            print(f"Failed to send message: {response.status_code}")
 
 def fetch_preview(url):
     try:
@@ -117,29 +141,52 @@ def fetch_preview(url):
     except Exception as e:
         return str(e)
     
-def send_discord_message_passes(webhook_url, feed_name, feed_icon, color, tags, image, entry, waPass):
+def send_discord_message_passes(webhook_url, feed_name, feed_icon, color, tags, entry, waPass, summery):
     try:
         title = entry.title
     except AttributeError:
         title = feed_name
 
+    desc = f"{summery['Directions'][0][1]} \n {summery['Conditions']}"
+    formated_pic = f"{waPass['Camera']}?a={int(time.time())}"
+    formated_username = f"WSDoT: {waPass['Pass']} - Elevation: {waPass['Elevation']}"
+
+    if 'closed' in entry.summary.lower():
+        status = "Closed"
+    else:
+        status = "Open"
+
     data = {
-        "username": feed_name,
+        "username": formated_username,
         "avatar_url": feed_icon,
         "embeds": [
             {
             "title": title, 
             "url": entry.link,
-            "description": entry.summary,
+            "description": desc,
             "color": color,
             "fields": [
                 {
-                    "name": "Tags",
-                    "value": tags
+                    "name": "Temperature",
+                    "value": summery['Temperature'],
+                    "inline": True
+                },
+                {
+                    "name": "Weather",
+                    "value": summery['Weather'],
+                    "inline": True
+                },
+                {
+                    "name": "Status",
+                    "value": status,
+                    "inline": True
                 }
             ],
             "image": {
-                "url": waPass['Camera']
+                "url": formated_pic
+            },
+            "footer": {
+                "text": f"Last Updated{summery['Datetime'].strftime('%m/%d/%Y %I:%M %p')} PST",
             }
             }
 
@@ -151,6 +198,14 @@ def send_discord_message_passes(webhook_url, feed_name, feed_icon, color, tags, 
         print("Message sent successfully")
     else:
         print(f"Failed to send message: {response.status_code}")
+
+
+def find_partial_match(passes_list, title):
+    pass_name = title.split(' ')[0]
+    for pass_info in passes_list:
+        if pass_name in pass_info['Pass']:
+            return pass_info
+    return None 
 
 passes = load_pass_info()
 
@@ -171,14 +226,13 @@ for feed in feed_info:
 
         for entry in new_entries:
             tags = ""
-            image = fetch_preview(entry.link)
-            time.sleep(1)
             if feed_name == "WSDoT Highway Alerts":
-                send_discord_message_road(webhook, feed_name, feed_icon, feed_color, tags, image, entry)
+                send_discord_message_road(webhook, feed_name, feed_icon, feed_color, tags, entry)
             else:
-                for waPass in passes:
-                    if waPass['Pass'] in feed_name:
-                        print(f"Sending new entry: {entry.title}")
-                        send_discord_message_passes(webhook, feed_name, feed_icon, feed_color, tags, image, entry, waPass)
+                waPass = find_partial_match(passes, entry.title)
+                if waPass is not None:
+                    print(f"Sending new entry: {entry.title}")
+                    summery = parse_html_passes(entry.summary)
+                    send_discord_message_passes(webhook, feed_name, feed_icon, feed_color, tags, entry, waPass, summery)
                 
 
